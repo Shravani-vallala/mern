f18d6fe35e948042a28d6b087e516018405623e9
SwapnaJakkali <swapnajakkali25@gmail.com>
2025-08-12 20:49:55 +0530

    signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import HeroSection from './components/HeroSection';
-
-
+import SignIn from './pages/SignIn'; // Assuming you saved your SignIn in pages folder
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <HeroSection />
-      <Home/>
-    </>
+      <Routes>
+        {/* Home Route */}
+        <Route path="/" element={
+          <>
+            <HeroSection />
+            <Home />
+          </>
+        } />
+        
+        {/* Sign In Route */}
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   );
 }
 
